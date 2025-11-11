8d04ed9d5a4a868d5bf572b9692eabd8f2f1b940
jiyongchoi70 <jiyongchoi70@gmail.com>
2025-11-11 10:57:35 +0900

    config.js 諛고룷??異붽?

---
+++ public/config.js
@@ -0,0 +1,5 @@
+﻿window.APP_CONFIG = {
+  supabaseUrl: "https://uwasbrfsgvlmkmgtaotm.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3YXNicmZzZ3ZsbWttZ3Rhb3RtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NTMyMDksImV4cCI6MjA3ODMyOTIwOX0.HTn33T8ioloEsG0wHzOT5YhatGveJ3MvNJ5OK1qW8JM",
+  hintEndpoint: "../server/gpt-helper.php"
+};
